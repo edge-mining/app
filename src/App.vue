5bcf865e93137215b35301a6a723dd95bf43e986
sebailfico <sebailfico@yahoo.it>
2025-08-26 09:44:04 +0200

    Adding VueApp from scratch + tailwind + daisyUI

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
+<script setup lang="ts"></script>
+
 <template>
-  <ThemeProvider>
-    <SidebarProvider>
-      <RouterView />
-    </SidebarProvider>
-  </ThemeProvider>
+  <RouterView />
 </template>
 
-<script setup lang="ts">
-import ThemeProvider from './ui_kit/tailadmin/layout/ThemeProvider.vue'
-import SidebarProvider from './ui_kit/tailadmin/layout/SidebarProvider.vue'
-</script>
+<style scoped>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,6 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router/indext";
 
-// Import Swiper styles
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import 'jsvectormap/dist/jsvectormap.css'
-import 'flatpickr/dist/flatpickr.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia' // Import Pinia
-
-import App from './App.vue'
-import router from './router'
-
-import VueApexCharts from 'vue3-apexcharts'
-
-const pinia = createPinia()
-const app = createApp(App)
-
-app.use(pinia)
-app.use(router)
-app.use(VueApexCharts)
-
-app.mount('#app')
+createApp(App).use(router).mount("#app");
