03bc8a44b1060ef47e329dd578b2e8c27dc8765d
markoceri <mancinomarko@hotmail.it>
2025-10-17 12:16:15 +0200

    feat: add EnergyLoadHistoryProviderAdapter

--- edge_mining/domain/home_load/common.py
+++ edge_mining/domain/home_load/common.py
@@ -9,3 +9,10 @@ class HomeForecastProviderAdapter(AdapterType):
     """Types of home forecast provider adapter."""
 
     DUMMY = "dummy"
+
+
+class EnergyLoadHistoryProviderAdapter(AdapterType):
+    """Types of energy load history provider adapter."""
+
+    DUMMY = "dummy"
+    HOME_ASSISTANT_API = "home_assistant_api"
