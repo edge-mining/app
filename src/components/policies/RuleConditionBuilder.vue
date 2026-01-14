2f9812f1ef289ba47fc77025e909ed152cb4bd0b
Marco Mancino <mancinomarko@hotmail.it>
2026-01-14 19:01:14 +0100

    refactor: replace SVG icon with PhX component in RuleConditionBuilder

--- src/components/policies/RuleConditionBuilder.vue
+++ src/components/policies/RuleConditionBuilder.vue
@@ -722,9 +722,7 @@ const handleValueInput = (event: Event) => {
                 :disabled="groupItems.length === 1 || groupType === 'not_'"
                 :title="groupItems.length === 1 ? 'Cannot remove the last item' : groupType === 'not_' ? 'NOT group must have exactly one item' : 'Remove this condition'"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
+                <PhX :size="16" />
               </button>
             </div>
           </div>
