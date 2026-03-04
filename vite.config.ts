3d43a69cce1e59f53471270166f7eee597e9a983
Marco Mancino <mancinomarko@hotmail.it>
2026-03-04 15:20:33 +0100

    fix: cannot find module fs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-import { readFileSync } from "fs";
-
-const packageJson = JSON.parse(readFileSync("./package.json", "utf-8"));
+import packageJson from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
