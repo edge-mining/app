72f575fe8c0d433111b7a0d03c27babf0a6462a3
markoceri <mancinomarko@hotmail.it>
2026-04-17 23:06:19 +0200

    feat: add PayoutFrequency enum and update MiningPerformanceTrackerAdapter with new types

--- edge_mining/domain/performance/common.py
+++ edge_mining/domain/performance/common.py
@@ -1,5 +1,6 @@
 """Collection of Common Objects for the Mining Performance Analysis domain of the Edge Mining application."""
 
+from enum import Enum
 from typing import NewType
 
 from edge_mining.domain.common import AdapterType
@@ -12,3 +13,15 @@ class MiningPerformanceTrackerAdapter(AdapterType):
     """Types of mining performance tracker adapter."""
 
     DUMMY = "dummy"
+    OCEAN = "ocean"
+    BRAIINS_POOL = "braiins_pool"
+
+
+class PayoutFrequency(str, Enum):
+    """How often a pool issues payouts."""
+
+    UNKNOWN = "unknown"
+    PER_BLOCK = "per_block"
+    HOURLY = "hourly"
+    DAILY = "daily"
+    THRESHOLD = "threshold"
